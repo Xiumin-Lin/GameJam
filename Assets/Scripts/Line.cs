d43c1ac520059ff60c8b8f7609d6b622a17f316c
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2023-09-21 19:11:10 +0200

    trigger

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,16 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Line : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] private KeyCode[] keys;
+    private Renderer _skin;
+    
+    private void Start()
+    {
+        _skin = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
     {
         Tile go = other.gameObject.GetComponent<Tile>();
         if (go != null)
         {
-            AudioManager.instance.PlayPianoAudio(go.ID);
-            Destroy(other.gameObject, 0);
+            // bool isKeyDown = false;
+            // foreach (KeyCode key in keys)
+            // {
+            //     if (Input.GetKeyDown(key)) isKeyDown = true;
+            // }
+
+            // if (isKeyDown)
+            {
+                AudioManager.instance.PlayPianoAudio(go.ID);
+                Destroy(other.gameObject, 0);
+                _skin.material.color = Color.green;
+            }
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit Tile!");
+        Destroy(other.gameObject, 1);
+        _skin.material.color = Color.red;
     }
 }
